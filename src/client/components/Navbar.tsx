b3a6c8302aea5af6926cba11d787cf1cef2923f9
Paula <pauladzuck@hotmail.com>
2021-12-01 17:08:31 -0600

    Adding update to navbar

--- src/client/components/Navbar.tsx
+++ src/client/components/Navbar.tsx
@@ -27,19 +27,37 @@ const Navbar: React.FC<NavbarProps> = () => {
                 <nav className="fixed-top nav-margin bg-white navbar navbar-expand-lg navbar-light border border-top-0 border-left-0 border-right-0 shadow-sm pr-5  ">
                     <a className="navbar-brand" href="/"> Subtle Stillness</a>
 
-                    {/* <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
-                    </button> */}
-{/* 
-                    <div className="collapse navbar-collapse" id="navbarSupportedContent"> */}
-                        <ul className="navbar-nav ml-auto">
+                    </button>
+
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+                        <ul className="navbar-nav-desktop ml-auto">
                             <ul className="nav nav-pills">
                                 <li className="nav-item">
                                     <a className="nav-link active" href="/login">Sign Up</a>
                                 </li>
                             </ul>
+
                         </ul>
-                    {/* </div> */}
+
+                        <ul className="navbar-nav-mobile ml-auto">
+                            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                                <li className="nav-item">
+                                    <a className="nav-link active" href="/login">Sign Up</a>
+                                </li>
+                                <li className="nav-item">
+                                    <a className="nav-link" href="/login">Sign Up</a>
+                                </li>
+                            </ul>
+
+                        </ul>
+
+                      
+                      
+                    </div>
 
                 </nav>
 
@@ -79,6 +97,28 @@ const Navbar: React.FC<NavbarProps> = () => {
 
 const Background = styled.div`
 
+@media (max-width:  979px) {
+	.navbar-nav-desktop {
+		display: none;
+	}
+}
+
+
+@media (min-width:  979px) {
+	.navbar-nav-mobile {
+		display: none;
+	}
+}
+
+.navbar-brand {
+    font-family: 'Beth Ellen', sans-serif;
+    font-size: 20px;     
+}
+
+
+
+
+
 .navbar-brand {
     font-family: 'Beth Ellen', sans-serif;
     font-size: 20px;     
@@ -88,11 +128,10 @@ const Background = styled.div`
     .nav-link {
       @include border-radius($nav-pills-border-radius);
     }
-  
+
     .nav-link.active,
     .show > .nav-link {
       color: grey;
-    //   outline-color: #93E9BE;
       background-color: white;
       border: 2px;
       outline-style: solid;
@@ -102,10 +141,14 @@ const Background = styled.div`
   }
 
 
-
+ 
 
 
 `;
+
+
+
+
 
 
 
